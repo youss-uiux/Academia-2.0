162c318c74b7aef7780c218d4fd611dd4065c361
youss-uiux <youssif.aboubacar@outlook.fr>
2025-05-30 23:46:42 +0200

    refactor course details component to load media files dynamically and improve layout

--- src/app/pages/course-details/course-details.component.ts
+++ src/app/pages/course-details/course-details.component.ts
@@ -11,15 +11,61 @@ export class CourseDetailsComponent implements OnInit {
 
   course!: Course;
 
+  imageBlobUrl: string | null = null;
+  videoBlobUrl: string | null = null;
+  audioBlobUrl: string | null = null;
+  pdfBlobUrl: string | null = null;
+
   constructor(private route: ActivatedRoute, private courseService: CourseDetailsService) {}
 
   ngOnInit(): void {
     const courseId = Number(this.route.snapshot.paramMap.get('id'));
 
     this.courseService.getCourseById(courseId).subscribe({
-      next: (data) => this.course = data,
+      next: (data) =>{
+        this.course = data;
+       // Ensuite, pour chaque média, on traite séparément
+       this.loadMedia(this.course.imageUrl, 'image');
+       this.loadMedia(this.course.videoUrl, 'video');
+       this.loadMedia(this.course.audioUrl, 'audio');
+       this.loadMedia(this.course.pdfUrl, 'pdf');
+      } ,
       error: (err) => console.error(err)
     });
+  }
+
+
+  extractMediaInfo(url: string): { type: string, filename: string } | null {
+    const regex = /\/media\/(video|audio|pdf|image)\/([^\/]+)$/;
+    const match = url.match(regex);
+  
+    if (match) {
+      const type = match[1];
+      const filename = match[2];
+      return { type, filename };
+    }
+  
+    return null;
+  }
+
+  loadMedia(url: string, expectedType: string): void {
+    if (!url) return;
+
+    const mediaInfo = this.extractMediaInfo(url);
+    if (mediaInfo && mediaInfo.type === expectedType) {
+      this.courseService.getMedia(mediaInfo.type, mediaInfo.filename).subscribe({
+        next: (blob) => {
+          const objectUrl = URL.createObjectURL(blob);
+          switch (expectedType) {
+            case 'image': this.imageBlobUrl = objectUrl; break;
+            case 'video': this.videoBlobUrl = objectUrl; break;
+            case 'audio': this.audioBlobUrl = objectUrl; break;
+            case 'pdf': this.pdfBlobUrl = objectUrl; break;
+          }
+        },
+        error: (err) => console.error(`Erreur lors du chargement du ${expectedType}:`, err)
+      });
+    }
   }
 }
 
